ec8b2e6bea67fc1a337a2ed87c4713c72d685c8e
Rudi Persson <rudi0234@edu.easj.dk>
2016-11-17 12:25:05 +0100

    2and commit

--- Crossfit/ViewModel/WodViewModel.cs
+++ Crossfit/ViewModel/WodViewModel.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Crossfit.ViewModel
 {
-    public class WodViewModel
+    public class WodViewModel : INotifyPropertyChanged
     {
 
         public Model.WodList Wodliste { get; set; }
 
         private Model.Wod _selectedWod;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public Model.Wod SelectedWod
         {
             get { return _selectedWod; }
-            set { _selectedWod = value; }
+            set { _selectedWod = value;
+                OnPropertyChanged(nameof(SelectedWod));
+            }
         }
 
 
@@ -24,6 +29,15 @@ namespace Crossfit.ViewModel
         {
             Wodliste = new Model.WodList();
             _selectedWod = new Model.Wod();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
